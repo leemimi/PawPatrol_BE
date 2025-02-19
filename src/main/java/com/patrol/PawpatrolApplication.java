eb3caebb6c6bae85ba73b9b2325e804899f46d8b
leemimi <mijeong1015@naver.com>
2025-02-19 12:23:20 +0900

    First Setting

---
+++ src/main/java/com/patrol/PawpatrolApplication.java
@@ -0,0 +1,13 @@
+package com.patrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawpatrolApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(PawpatrolApplication.class, args);
+    }
+
+}
