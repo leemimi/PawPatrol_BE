4eeb23b004bfa9cdd1e7979c0aee580d3eae67ec
backnback <garam8796@naver.com>
2025-02-21 10:36:27 +0900

    fix: fix

--- src/main/java/com/patrol/domain/protection/protection/repository/ProtectionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.patrol.domain.protection.protection.repository;
-
-import com.patrol.domain.protection.protection.entity.Protection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProtectionRepository extends JpaRepository<Protection, Long> {
-  
-}
