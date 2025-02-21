f8cf0b79763bf56452bf5865c017da5e9a1306a9
ksuju <139477852+ksuju@users.noreply.github.com>
2025-02-21 14:26:07 +0900

    feat: 현재 로그인 사용자 정보 조회 (내 정보 조회) 메서드 추가

--- src/main/java/com/patrol/api/member/auth/controller/ApiV2AuthController.java
+++ src/main/java/com/patrol/api/member/auth/controller/ApiV2AuthController.java
@@ -2,19 +2,18 @@ package com.patrol.api.member.auth.controller;
 
 import com.patrol.api.member.auth.dto.request.SignupRequest;
 import com.patrol.api.member.auth.dto.requestV2.LoginRequest;
+import com.patrol.api.member.auth.dto.requestV2.LoginUserInfoResponse;
 import com.patrol.domain.member.auth.service.V2AuthService;
 import com.patrol.domain.member.member.entity.Member;
 import com.patrol.domain.member.member.service.V2MemberService;
 import com.patrol.global.globalDto.GlobalResponse;
 import com.patrol.global.rq.Rq;
+import com.patrol.global.webMvc.LoginUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * packageName    : com.patrol.api.member.auth.controller
@@ -61,5 +60,16 @@ public class ApiV2AuthController {
         rq.deleteCookie("apiKey");
         logger.info("로그아웃");
         return GlobalResponse.success();
+    }
+
+    // 현재 로그인 사용자 정보 조회
+    @GetMapping("/me")
+    public GlobalResponse<LoginUserInfoResponse> loginUserInfo(@LoginUser Member member) {
+
+        LoginUserInfoResponse userInfo = LoginUserInfoResponse.builder()
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .build();
+        return GlobalResponse.success(userInfo);
     }
 }
