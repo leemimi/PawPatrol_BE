685dab4b9fadc5758db17cd88716bf39cad091c8
ksuju <139477852+ksuju@users.noreply.github.com>
2025-02-23 19:12:46 +0900

    chore: 사용하지 않는 scope 제거

--- src/main/java/com/patrol/global/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/patrol/global/oauth2/CustomOAuth2UserService.java
@@ -2,7 +2,7 @@ package com.patrol.global.oauth2;
 
 
 
-import com.patrol.domain.member.auth.service.AuthService;
+import com.patrol.domain.member.auth.service.V2AuthService;
 import com.patrol.domain.member.member.entity.Member;
 import com.patrol.domain.member.member.enums.ProviderType;
 import com.patrol.domain.member.member.service.MemberService;
@@ -11,18 +11,11 @@ import com.patrol.global.exceptions.ErrorCodes;
 import com.patrol.global.exceptions.ServiceException;
 import com.patrol.global.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Locale;
@@ -33,7 +26,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-  private final AuthService authService;
+  private final V2AuthService v2AuthService;
   private final MemberService memberService;
   private final SocialConnectService socialConnectService;
 
@@ -51,30 +44,22 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         .toUpperCase(Locale.getDefault()));
 
     String email;
-    String nickname;
-    String profileImageUrl;
 
     switch (loginType) {
       case KAKAO -> {
         Map<String, Object> attributes = oAuth2User.getAttributes();
         Map<String, String> properties = (Map<String, String>) attributes.get("properties");
-        nickname = properties.get("nickname");
-        profileImageUrl = properties.get("profile_image");
         Map<String, String> kakaoAccount = (Map<String, String>) attributes.get("kakao_account");
         email = kakaoAccount.get("email");
       }
 
       case GOOGLE -> {
         email = oAuth2User.getAttribute("email");
-        nickname = oAuth2User.getAttribute("name");
-        profileImageUrl = oAuth2User.getAttribute("picture");
       }
 
       case NAVER -> {
         Map<String, Object> response = (Map<String, Object>) oAuth2User.getAttributes().get("response");
         email = (String) response.get("email");
-        nickname = (String) response.get("nickname");
-        profileImageUrl = (String) response.get("profile_image");
         oauthId = (String) response.get("id");
       }
 
@@ -92,7 +77,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
       Member loginUser = memberService.findById(originMemberId)
           .orElseThrow(() -> new ServiceException(ErrorCodes.MEMBER_NOT_FOUND));
 
-      authService.connectOAuthProvider(loginUser, loginType, providerId, email);
+      v2AuthService.connectOAuthProvider(loginUser, loginType, providerId, email);
       // Redis 서버에 멤버 정보가 남아있을 필요가 없으므로 삭제 (토큰으로 저장됨)
       socialConnectService.removeOrigin();
 
@@ -106,12 +91,10 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
       );
 
     } else {  // 사이트 자체 계정이 존재 X
-
-      Member member = authService.handleSocialLogin(
-          email, nickname, loginType, profileImageUrl, providerId
-      );
-
-      return new SecurityUser(
+        Member member = v2AuthService.handleSocialLogin(
+                email, loginType, providerId
+        );
+        return new SecurityUser(
           member.getId(),
           member.getEmail(),
           member.getPassword(),
@@ -121,30 +104,4 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
       );
     }
   }
-
-
-//  private String _getGithubPrivateEmail(OAuth2AccessToken accessToken) {
-//    try {
-//      RestTemplate restTemplate = new RestTemplate();
-//      HttpHeaders headers = new HttpHeaders();
-//      headers.setBearerAuth(accessToken.getTokenValue());
-//
-//      HttpEntity<?> entity = new HttpEntity<>(headers);
-//      ResponseEntity<List<Map<String, Object>>> response = restTemplate.exchange(
-//          "https://api.github.com/user/emails",
-//          HttpMethod.GET,
-//          entity,
-//          new ParameterizedTypeReference<List<Map<String, Object>>>() {}
-//      );
-//
-//      return response.getBody().stream()
-//          .filter(emailObj -> (Boolean) emailObj.get("primary") && (Boolean) emailObj.get("verified"))
-//          .map(emailObj -> (String) emailObj.get("email"))
-//          .findFirst()
-//          .orElse(null);
-//
-//    } catch (Exception e) {
-//      return null;
-//    }
-//  }
 }
