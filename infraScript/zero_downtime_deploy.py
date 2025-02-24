ef281f36dc23044c52a41782905a0c937aa4237e
leemimi <mijeong1015@naver.com>
2025-02-24 15:02:37 +0900

    test:deploy

--- infraScript/zero_downtime_deploy.py
+++ infraScript/zero_downtime_deploy.py
@@ -46,7 +46,7 @@ class ServiceManager:
     # Docker 컨테이너를 실행하는 함수
     def _run_container(self, name: str, port: int) -> None:
         os.system(
-            f"docker run -d --name={name} --restart unless-stopped -p {port}:8090 -e TZ=Asia/Seoul -v /dockerProjects/pawpatrol/volumes/gen:/gen --pull always ghcr.io/BackEndSchoolPlus3th/PawPatrol_BE")
+            f"docker run -d --name={name} --restart unless-stopped -p {port}:8090 -e TZ=Asia/Seoul -v /dockerProjects/pawpatrol/volumes/gen:/gen --pull always ghcr.io/backendschoolplus3th/pawpatrol_be")
 
 
     def _switch_port(self) -> None:
