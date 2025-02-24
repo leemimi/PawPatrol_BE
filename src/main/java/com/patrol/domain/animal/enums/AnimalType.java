6afbeb80624ca4a5e8be4eb4af580f636d1f4a0f
ksuju <139477852+ksuju@users.noreply.github.com>
2025-02-24 12:25:42 +0900

    feat: 반려동물 등록을 위해 필요한 AnimalType Enum 추가

---
+++ src/main/java/com/patrol/domain/animal/enums/AnimalType.java
@@ -0,0 +1,19 @@
+package com.patrol.domain.animal.enums;
+
+import lombok.Getter;
+
+/**
+ * packageName    : com.patrol.domain.animal.enums
+ * fileName       : AnimalType
+ * author         : sungjun
+ * date           : 2025-02-24
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-02-24        kyd54       최초 생성
+ */
+@Getter
+public enum AnimalType {
+    CAT, DOG
+}
