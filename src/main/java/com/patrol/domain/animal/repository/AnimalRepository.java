09274f565b6db0b5892c3a72db500986128fbd25
ksuju <139477852+ksuju@users.noreply.github.com>
2025-02-24 14:38:00 +0900

    feat: AnimalRepository 추가

---
+++ src/main/java/com/patrol/domain/animal/repository/AnimalRepository.java
@@ -0,0 +1,18 @@
+package com.patrol.domain.animal.repository;
+
+import com.patrol.domain.animal.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * packageName    : com.patrol.domain.animal.repository
+ * fileName       : AnimalRepository
+ * author         : sungjun
+ * date           : 2025-02-24
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-02-24        kyd54       최초 생성
+ */
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+}
