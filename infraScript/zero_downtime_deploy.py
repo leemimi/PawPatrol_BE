53a3c8cb74f7915fcd1e8a6391b26a0f75bb05a1
leemimi <mijeong1015@naver.com>
2025-02-24 15:27:40 +0900

    test:deploy

--- infraScript/zero_downtime_deploy.py
+++ infraScript/zero_downtime_deploy.py
@@ -13,8 +13,8 @@ class ServiceManager:
         self.socat_port: int = socat_port
         self.sleep_duration: int = sleep_duration
         self.services: Dict[str, int] = {
-            'pawpatrol_be_1': 8082,
-            'pawpatrol_be_2': 8083
+            'pawpatrol_1': 8082,
+            'pawpatrol_2': 8083
         }
         self.current_name: Optional[str] = None
         self.current_port: Optional[int] = None
@@ -26,7 +26,7 @@ class ServiceManager:
         cmd: str = f"ps aux | grep 'socat -t0 TCP-LISTEN:{self.socat_port}' | grep -v grep | awk '{{print $NF}}'"
         current_service: str = subprocess.getoutput(cmd)
         if not current_service:
-            self.current_name, self.current_port = 'pawpatrol_be_2', self.services['pawpatrol_be_2']
+            self.current_name, self.current_port = 'pawpatrol_2', self.services['pawpatrol_2']
         else:
             self.current_port = int(current_service.split(':')[-1])
             self.current_name = next((name for name, port in self.services.items() if port == self.current_port), None)
@@ -46,7 +46,7 @@ class ServiceManager:
     # Docker 컨테이너를 실행하는 함수
     def _run_container(self, name: str, port: int) -> None:
         os.system(
-            f"docker run -d --name={name} --restart unless-stopped -p {port}:8090 -e TZ=Asia/Seoul -v /dockerProjects/pawpatrol/volumes/gen:/gen --pull always ghcr.io/BackEndSchoolPlus3th/PawPatrol_BE")
+            f"docker run -d --name={name} --restart unless-stopped -p {port}:8090 -e TZ=Asia/Seoul -v /dockerProjects/pawpatrol/volumes/gen:/gen --pull always ghcr.io/backendschoolplus3th/pawpatrol")
 
 
     def _switch_port(self) -> None:
