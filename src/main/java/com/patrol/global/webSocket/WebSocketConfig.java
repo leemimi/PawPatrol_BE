6f6ddd38de298c2449a287e74a334057efe47346
junghyun13 <ljh041314@gmail.com>
2025-02-27 20:57:27 +0900

    글작성시 알림

---
+++ src/main/java/com/patrol/global/webSocket/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.patrol.global.webSocket;
+
+import com.patrol.global.app.AppConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins(AppConfig.getSiteFrontUrl(), AppConfig.getDevFrontUrl())
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue", "/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+}
