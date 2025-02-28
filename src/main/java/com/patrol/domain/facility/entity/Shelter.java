99f82c665df5757f9168143f6a69b7720057cc30
backnback <garam8796@naver.com>
2025-02-28 15:42:18 +0900

    feat: 시설 entity 수정

--- src/main/java/com/patrol/domain/facility/entity/Shelter.java
+++ src/main/java/com/patrol/domain/facility/entity/Shelter.java
@@ -1,11 +1,19 @@
 package com.patrol.domain.facility.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.patrol.domain.animalCase.entity.AnimalCase;
+import com.patrol.domain.member.member.entity.Member;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.ALL;
 
 @Entity
 @Getter
@@ -17,4 +25,13 @@ public class Shelter extends Facility {
 
   private Integer vetPersonCount;  // 수의사 수
   private String saveTargetAnimal; // 구조대상동물
+
+  @JsonIgnore
+  @OneToOne(mappedBy = "shelter", cascade = ALL, orphanRemoval = true)
+  private Member shelterMember;
+
+  @JsonIgnore
+  @OneToMany(mappedBy = "shelter", cascade = CascadeType.ALL, orphanRemoval = true)
+  @Builder.Default
+  private List<AnimalCase> animalCases = new ArrayList<>();
 }
