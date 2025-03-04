7bb7314cdfc1870160197f49533ae172cca9e574
ksuju <139477852+ksuju@users.noreply.github.com>
2025-03-05 00:44:42 +0900

    feat: 사업자등록번호 검증 위한 클래스 추가

---
+++ src/main/java/com/patrol/api/member/auth/dto/requestV2/ShelterSignupRequest.java
@@ -0,0 +1,22 @@
+package com.patrol.api.member.auth.dto.requestV2;
+
+/**
+ * packageName    : com.patrol.api.member.auth.dto.requestV2
+ * fileName       : ShelterSignupRequest
+ * author         : sungjun
+ * date           : 2025-03-04
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-03-04        kyd54       최초 생성
+ */
+public record ShelterSignupRequest(
+        String email,
+        String password,
+        String owner,
+        String address,
+        String businessRegistrationNumber
+) {
+
+}
