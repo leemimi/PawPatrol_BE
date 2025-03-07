93dc0f933cea217256a85c44191c4c2569f697f0
ksuju <139477852+ksuju@users.noreply.github.com>
2025-03-07 12:09:28 +0900

    fix: 보호소 마이페이지 반려동물 무한스크롤 적용을 위한 Pageable 적용

--- src/main/java/com/patrol/domain/animal/repository/AnimalRepository.java
+++ src/main/java/com/patrol/domain/animal/repository/AnimalRepository.java
@@ -1,9 +1,10 @@
 package com.patrol.domain.animal.repository;
 
 import com.patrol.domain.animal.entity.Animal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -18,7 +19,7 @@ import java.util.Optional;
  * 2025-02-24        kyd54       최초 생성
  */
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
-    List<Animal> findByOwnerId(Long ownerId);
+    Page<Animal> findByOwnerId(Long ownerId, Pageable pageable);
 
     Optional<Animal> findByRegistrationNo(String animalNo);
 }
