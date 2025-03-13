a88b0d66abef2a9f3a882ac4c472f31e172ff018
ksuju <139477852+ksuju@users.noreply.github.com>
2025-03-13 17:24:25 +0900

    chore: notProd 주석처리

--- src/main/java/com/patrol/global/initData/NotProd.java
+++ src/main/java/com/patrol/global/initData/NotProd.java
@@ -72,96 +72,96 @@ public class NotProd {
         memberRepository.save(mem1);
 
 
-        List<PetRegisterRequest> sampleAnimals = SampleAnimalData.getSampleStrayAnimals();
-        List<String> imageUrls = SampleAnimalData.getSampleImageUrls();
-        List<String> animalNameList = SampleAnimalData.getAnimalNameList();
-
-        // 생성된 동물들을 저장할 리스트
-        List<Animal> animalList = new ArrayList<>();
-
-        // 동물 생성 (40마리)
-        int animalCount = Math.min(40, sampleAnimals.size());
-        for (int i = 0; i < animalCount; i++) {
-          PetRegisterRequest animalRequest = sampleAnimals.get(i);
-          String imageUrl = imageUrls.get(i);
-          String name = animalNameList.get(i);
-
-          Animal animal = animalService.registerWithImageUrl(animalRequest, imageUrl);
-          animal.setName(name);
-          animal.setLost(false);
-          animalList.add(animal);
-
-          System.out.println("샘플 데이터 생성: " + animal.getName() +
-              " - " + animal.getBreed() + " (" + animal.getImageUrl() + ")");
-        }
-
-        // 임시보호용 게시글 타이틀 가져오기
-        List<String> protectionTitles = SampleAnimalData.getSampleTitles();
-
-        // member1 - 20마리
-        int casesForMember1 = Math.min(20, animalList.size());
-        for (int i = 0; i < casesForMember1; i++) {
-          animalCaseEventPublisher.createAnimalCase(
-              member1, animalList.get(i), protectionTitles.get(i), null, "서울 마포구"
-          );
-          System.out.println("Member1의 동물 케이스 생성: " + animalList.get(i).getBreed());
-        }
-
-        // member2 - 10마리
-        int casesForMember2 = Math.min(10, animalList.size() - casesForMember1);
-        for (int i = 0; i < casesForMember2; i++) {
-          int index = casesForMember1 + i;
-          if (index < animalList.size()) {
-            animalCaseEventPublisher.createAnimalCase(
-                member2, animalList.get(index), protectionTitles.get(index), null, "서울 송파구"
-            );
-            System.out.println("Member2의 동물 케이스 생성: " + animalList.get(index).getBreed());
-          }
-        }
-
-        // member3 - 10마리
-        int startIndexForMember3 = casesForMember1 + casesForMember2;
-        int casesForMember3 = Math.min(10, animalList.size() - startIndexForMember3);
-        for (int i = 0; i < casesForMember3; i++) {
-          int index = startIndexForMember3 + i;
-          if (index < animalList.size()) {
-            animalCaseEventPublisher.createAnimalCase(
-                member3, animalList.get(index), protectionTitles.get(index), null, "경기도 안양시"
-            );
-            System.out.println("Member3의 동물 케이스 생성: " + animalList.get(index).getBreed());
-          }
-        }
+//        List<PetRegisterRequest> sampleAnimals = SampleAnimalData.getSampleStrayAnimals();
+//        List<String> imageUrls = SampleAnimalData.getSampleImageUrls();
+//        List<String> animalNameList = SampleAnimalData.getAnimalNameList();
+//
+//        // 생성된 동물들을 저장할 리스트
+//        List<Animal> animalList = new ArrayList<>();
+//
+//        // 동물 생성 (40마리)
+//        int animalCount = Math.min(40, sampleAnimals.size());
+//        for (int i = 0; i < animalCount; i++) {
+//          PetRegisterRequest animalRequest = sampleAnimals.get(i);
+//          String imageUrl = imageUrls.get(i);
+//          String name = animalNameList.get(i);
+//
+//          Animal animal = animalService.registerWithImageUrl(animalRequest, imageUrl);
+//          animal.setName(name);
+//          animal.setLost(false);
+//          animalList.add(animal);
+//
+//          System.out.println("샘플 데이터 생성: " + animal.getName() +
+//              " - " + animal.getBreed() + " (" + animal.getImageUrl() + ")");
+//        }
+//
+//        // 임시보호용 게시글 타이틀 가져오기
+//        List<String> protectionTitles = SampleAnimalData.getSampleTitles();
+//
+//        // member1 - 20마리
+//        int casesForMember1 = Math.min(20, animalList.size());
+//        for (int i = 0; i < casesForMember1; i++) {
+//          animalCaseEventPublisher.createAnimalCase(
+//              member1, animalList.get(i), protectionTitles.get(i), null, "서울 마포구"
+//          );
+//          System.out.println("Member1의 동물 케이스 생성: " + animalList.get(i).getBreed());
+//        }
+//
+//        // member2 - 10마리
+//        int casesForMember2 = Math.min(10, animalList.size() - casesForMember1);
+//        for (int i = 0; i < casesForMember2; i++) {
+//          int index = casesForMember1 + i;
+//          if (index < animalList.size()) {
+//            animalCaseEventPublisher.createAnimalCase(
+//                member2, animalList.get(index), protectionTitles.get(index), null, "서울 송파구"
+//            );
+//            System.out.println("Member2의 동물 케이스 생성: " + animalList.get(index).getBreed());
+//          }
+//        }
+//
+//        // member3 - 10마리
+//        int startIndexForMember3 = casesForMember1 + casesForMember2;
+//        int casesForMember3 = Math.min(10, animalList.size() - startIndexForMember3);
+//        for (int i = 0; i < casesForMember3; i++) {
+//          int index = startIndexForMember3 + i;
+//          if (index < animalList.size()) {
+//            animalCaseEventPublisher.createAnimalCase(
+//                member3, animalList.get(index), protectionTitles.get(index), null, "경기도 안양시"
+//            );
+//            System.out.println("Member3의 동물 케이스 생성: " + animalList.get(index).getBreed());
+//          }
+//        }
 
 // ==================== 내 반려동물 데이터 추가 ====================
-// 반려동물 샘플 데이터 가져오기
-        List<PetRegisterRequest> myPets = SampleMyPetData.getSamplePets();
-        List<String> myPetImageUrls = SampleMyPetData.getSampleImageUrls();
-
-// 각 회원별로 2마리씩 반려동물 등록
-        for (int i = 0; i < myPets.size(); i++) {
-          // 회원 할당 (0,1,2번 동물은 member2, 3,4,5번 동물은 member3)
-          Member owner = (i < 3) ? member2 : (i < 6) ? member3 : member1;
-
-          // Animal 객체 생성
-          Animal pet = Animal.builder()
-                  .owner(owner)
-                  .name(myPets.get(i).name())
-                  .registrationNo(myPets.get(i).registrationNo())
-                  .animalType(myPets.get(i).animalType())
-                  .breed(myPets.get(i).breed())
-                  .gender(myPets.get(i).gender())
-                  .size(myPets.get(i).size())
-                  .imageUrl(myPetImageUrls.get(i))
-                  .estimatedAge(myPets.get(i).estimatedAge())
-                  .healthCondition(myPets.get(i).healthCondition())
-                  .feature(myPets.get(i).feature())
-                  .build();
-
-          animalRepository.save(pet);
-
-          imageHandlerService.registerImage(myPetImageUrls.get(i), pet.getId(), null, null, pet.getAnimalType());
-          // Animal 객체 저장
-        }
+//// 반려동물 샘플 데이터 가져오기
+//        List<PetRegisterRequest> myPets = SampleMyPetData.getSamplePets();
+//        List<String> myPetImageUrls = SampleMyPetData.getSampleImageUrls();
+//
+//// 각 회원별로 2마리씩 반려동물 등록
+//        for (int i = 0; i < myPets.size(); i++) {
+//          // 회원 할당 (0,1,2번 동물은 member2, 3,4,5번 동물은 member3)
+//          Member owner = (i < 3) ? member2 : (i < 6) ? member3 : member1;
+//
+//          // Animal 객체 생성
+//          Animal pet = Animal.builder()
+//                  .owner(owner)
+//                  .name(myPets.get(i).name())
+//                  .registrationNo(myPets.get(i).registrationNo())
+//                  .animalType(myPets.get(i).animalType())
+//                  .breed(myPets.get(i).breed())
+//                  .gender(myPets.get(i).gender())
+//                  .size(myPets.get(i).size())
+//                  .imageUrl(myPetImageUrls.get(i))
+//                  .estimatedAge(myPets.get(i).estimatedAge())
+//                  .healthCondition(myPets.get(i).healthCondition())
+//                  .feature(myPets.get(i).feature())
+//                  .build();
+//
+//          animalRepository.save(pet);
+//
+//          imageHandlerService.registerImage(myPetImageUrls.get(i), pet.getId(), null, null, pet.getAnimalType());
+//          // Animal 객체 저장
+//        }
       }
     };
   }
