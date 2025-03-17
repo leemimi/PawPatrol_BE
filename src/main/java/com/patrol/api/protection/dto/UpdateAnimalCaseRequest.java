b8c224107106805ef2f8f477ff73303993c452bb
backnback <garam8796@naver.com>
2025-03-17 12:11:26 +0900

    chore: 주석제거

--- src/main/java/com/patrol/api/protection/dto/UpdateAnimalCaseRequest.java
+++ src/main/java/com/patrol/api/protection/dto/UpdateAnimalCaseRequest.java
@@ -7,18 +7,18 @@ import com.patrol.domain.animal.enums.AnimalType;
 import com.patrol.domain.animalCase.entity.AnimalCase;
 
 public record UpdateAnimalCaseRequest(
-    String title,             // 게시글 제목
-    String description,       // 게시글 상세 설명
-    String location,          // 지역
-    String breed,              // 품종
-    AnimalGender gender,       // 성별
-    AnimalSize size,          // 크기
-    String feature,           // 특징
-    String healthCondition,   // 건강상태
-    String name,              // 이름
-    String estimatedAge,      // 추정 나이
-    String registrationNo,    // 동물등록번호
-    AnimalType animalType     // 동물 종류 (강아지/고양이)
+    String title,
+    String description,
+    String location,
+    String breed,
+    AnimalGender gender,
+    AnimalSize size,
+    String feature,
+    String healthCondition,
+    String name,
+    String estimatedAge,
+    String registrationNo,
+    AnimalType animalType
 ) {
 
   public Animal updateAnimal(AnimalCase animalCase) {
